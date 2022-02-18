b4e4a9d6d12a552ebf29f7348d5a957b81d37536
adriFva <99723500+adriFva@users.noreply.github.com>
2022-02-18 12:12:39 +0100

    Funciones sin Window.onload
    Cuando estuve haciendo la función de validar el formulario he visto que solo ejecutaba la primera función de la lista y me decía que las otras funciones tenían un error en el .onclick.
    
    Me puse a ver donde estaba el error con la consola y me decía que solo estaba cargando la pagina donde estaba la primera función

--- js/funciones.js
+++ js/funciones.js
@@ -1,8 +1,34 @@
-window.onload = () => {
+/FORMULARIOS 
+// FUNCIONES DE CONTACTO
+    function validarContacto(){
+        const nombre = document.querySelector("#nom").value;
+        const email = document.querySelector("#email").value;
+        const sms = document.querySelector("#mensaje").value;
+        let errores = 0;
 
-    function registroVisible() {
-        document.querySelector("#traslado").classList.toggle("tarjeta");
+        if (nombre.length < 3 || nombre.length >20) {
+            errores ++;
+            console.log("error en nombre")
+        }
+        if (email.length < 0 || email.length > 20 || email.indexOf ("@") < 0 || email.indexOf(" ")>-1) {
+            errores ++;
+            console.log("error en email")
+        }
+        if (sms.length <= 0) {
+            errores++;
+            console.log("error en mensaje")
+        }
+        if (errores > 0){
+            alert ("Numero de errores: " + errores);
+            return false;
+        }
+        else{
+            alert("Mensaje Enviado");
+            return false;
+        }
     }
 
-    document.querySelector("#go_registro").onclick = registroVisible;
-}
+    //ANIMACIONES
+    function animreg(){
+        document.querySelector("#traslado").classList.toggle("tarjeta");
+    }
